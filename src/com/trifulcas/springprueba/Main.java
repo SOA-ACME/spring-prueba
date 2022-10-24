89efc50cf2d8be46bca2058ec225db56e3f2b921
Sebastian <desarrollo.espinacas1966@gmail.com>
2022-10-24 21:37:10 +0200

    Programa Java - Spring.  Uso básico de Spring con Beans definidos en XML.

---
+++ src/com/trifulcas/springprueba/Main.java
@@ -0,0 +1,31 @@
+package com.trifulcas.springprueba;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Cargar el contexto
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Pedir el bean
+		ISaludo miSaludo = context.getBean("miSaludo", Saludos.class);
+		
+		// Ejecutar el metodo
+		System.out.println(miSaludo.hola());
+		
+		// Pedir el bean
+		ISaludo miVecino = context.getBean("miVecino", Vecino.class);
+				
+		// Ejecutar el metodo
+		System.out.println(miVecino.hola());
+				
+		// Cerrar el contexto
+		context.close();
+
+	}
+
+}
---
+++ src/com/trifulcas/springprueba/Vecino.java
@@ -0,0 +1,16 @@
+package com.trifulcas.springprueba;
+
+public class Vecino implements ISaludo{
+	@Override
+	public String hola() {
+		// TODO Auto-generated method stub
+		return "Holi vecinito";
+	}
+	
+	@Override
+	public String adios() {
+		// TODO Auto-generated method stub
+		return "Hasta luegi!!";
+	}
+
+}
